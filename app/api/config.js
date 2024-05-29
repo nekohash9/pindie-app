0b111d35e111507fe375a8eb62a08c76e778e383
nekohash9 <nekonotgay@gmail.com>
2024-05-29 22:06:15 +0300

    config.js fix

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://pindie-backend-sadovnichy.nomorepartiesco.ru/' 
+export const BASE_URL = 'https://pindie-backend-sadovnichy.nomorepartiesco.ru' 
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
